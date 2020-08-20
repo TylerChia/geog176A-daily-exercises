5841b6a9d6d1ef84cf570fe65837fd63d94e5e55
Tyler Chia <tylerchia7@gmail.com>
2020-08-19 17:51:54 -0700

    daily exercise 11

--- R/day-11.R
+++ R/day-11.R
@@ -11,7 +11,9 @@ equal <- st_transform(my_cities, 5070)
 equidistant <- st_transform(my_cities, '+proj=eqdc +lat_0=40 +lon_0=-96 +lat_1=20 +lat_2=60 +x_0=0 +y_0=0 +datum=NAD83 +units=m +no_defs')
 
 st_distance(my_cities)
+st_distance(equal)
+st_distance(equidistant)
+
 st_distance(equal) %>%
   units::set_units("km") %>%
   units::drop_units()
-st_distance(equidistant)
