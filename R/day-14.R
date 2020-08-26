2f327e31b34c1043320f016f7424321824b6fdc8
Tyler Chia <tylerchia7@gmail.com>
2020-08-25 20:10:56 -0700

    daily exercise 14

---
+++ R/day-14.R
@@ -0,0 +1,43 @@
+library(tidyverse)
+library(USAboundaries)
+library(USAboundariesData)
+library(sf)
+library(rmapshaper)
+
+cities = readr::read_csv("uscities.csv") %>%
+  st_as_sf(coords = c("lng", "lat"), crs = 4326) %>%
+  st_transform(5070)
+
+get_conus = function(data, var){
+  filter(data, !get(var) %in%
+           c("Hawaii", "Puerto Rico", "Alaska"))
+}
+counties = st_transform(us_counties(), 5070) %>%
+  get_conus("state_name")
+
+point_in_polygon3 = function(points, polygon, id){
+  st_join(polygon, points) %>%
+    st_drop_geometry() %>%
+    count(.data[[id]]) %>%
+    setNames(c(id, "n")) %>%
+    left_join(polygon, by = id) %>%
+    st_as_sf()
+}
+
+plot_pip = function(data){
+  ggplot() +
+    geom_sf(data = data, aes(fill = log(n)), alpha = .9, size = .2) +
+    scale_fill_gradient(low = "white", high = "darkgreen") +
+    theme_void() +
+    theme(legend.position = 'none',
+          plot.title = element_text(face = "bold", color = "darkgreen", hjust = .5, size = 18)) +
+    labs(title = "Number of Cities in Each US County",
+         caption = paste0(sum(data$n), " locations represented"))
+}
+
+point_in_polygon3(cities, counties, "geoid") %>%
+  plot_pip() %>%
+  ggsave(file = "~/github/geog176A-daily-exercises/img/num_cities_per_county.png")
+
+
+
